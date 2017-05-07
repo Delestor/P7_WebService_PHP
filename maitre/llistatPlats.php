ad2bd44f3748c9e924661190aeb30b891997cba3
Albert Cadena <albert.cad.rub@gmail.com>
2017-05-07 19:37:53 +0200

    initial commit P7 webService PHP M15_UF2

--- maitre/llistatPlats.php
+++ maitre/llistatPlats.php
@@ -21,30 +21,47 @@ $connexio = new Connexio();
 
 $query = ("SELECT id, nom, kcal, tipus FROM plats WHERE tipus = 'primer'");
 $result = $connexio->query($query);
-cargarColumnasValores('Escull 3 primers', 'llistatPrimers[]',$result);
+cargarColumnasValores('Escull 3 primers', 'Primers',$result);
 
 
 $query = ("SELECT id, nom, kcal, tipus FROM plats WHERE tipus = 'segon'");
 $result = $connexio->query($query);
-cargarColumnasValores('Escull 3 segons', 'llistatSegons[]', $result);
+cargarColumnasValores('Escull 3 segons', 'Segons', $result);
 
 
 $query = ("SELECT id, nom, kcal, tipus FROM plats WHERE tipus = 'postre'");
 $result = $connexio->query($query);
-cargarColumnasValores('Escull 3 postres', 'llistatPostres[]',$result);
+cargarColumnasValores('Escull 3 postres', 'Postres',$result);
+
+
 
 function cargarColumnasValores($titolTaula, $nomSelect, $result) {
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
+        $listadoPlatos = array();
+        $count = 0;
         while ($row = mysqli_fetch_assoc($result)) {
-            print json_encode(array_map('utf8_encode',array(
+            /*print json_encode(array_map('utf8_encode',array(
                 'estado' => '1',
                 'id' => $row["id"], 
                 'nom' => $row["nom"], 
                 'kcal' => $row["kcal"],
-                'tipus' => $row["tipus"])));
+                'tipus' => $row["tipus"])));*/
+            
+            $listadoPlatos["resultado".$nomSelect.$count] = array_map('utf8_encode',['estado' => '1',
+                'id' => $row["id"], 
+                'nom' => $row["nom"], 
+                'kcal' => $row["kcal"],
+                'tipus' => $row["tipus"]]);
+            /*
+             array_push($listadoPlatos["resultado".$nomSelect], array_map('utf8_encode',['estado' => '1',
+                'id' => $row["id"], 
+                'nom' => $row["nom"], 
+                'kcal' => $row["kcal"],
+                'tipus' => $row["tipus"]]));*/
+            $count++;
         }
-
+        print json_encode($listadoPlatos);
     } else {
         $s = split(" " , $titolTaula);
         print json_encode(array('estado' => '2','mensaje' => 'No hi ha '.$s[2].' plats creats'));
